259b5e5281ec84e637d64e99cbac2b855beddcff
JORGETECH <jorgesanini@gmail.com>
2021-06-17 00:05:46 +0200

    Revert GPU clock maximum frequency to 500 Mhz
    Might be completely incorrect according to the CPU/GPU clock division

--- drivers/gpu/vivante/v4/gc_hal_kernel_driver.c
+++ drivers/gpu/vivante/v4/gc_hal_kernel_driver.c
@@ -696,7 +696,7 @@ static void enable_jzsoc_gpu_clock(void)
 #ifdef CONFIG_MACH_JZ4770
     {
         /* JZ4770 GPU CLK2x 360MHz -- 540MHz --this is the correct value for 1080Mhz overclock and 360Mhz downclock (in userspace the min value is 360Mhz), because the gpu clock is cpuclock/2 */
-#define GPU_CLK_MAX 540000000
+#define GPU_CLK_MAX 500000000
         unsigned int GPUCDR_VAL=0;
         int div;
         int gpu_use_pll1 = 1;
